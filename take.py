5fa36ed1d81fa8fe09224b2fcc83ef3c7a5bb085
Xiaosea <maysrp@gmail.com>
2019-07-26 16:28:37 +0800

    Create take.py

---
+++ take.py
@@ -0,0 +1,10 @@
+form machine import Pin
+import camera
+camera.init()
+flash_light=Pin(04,Pin.OUT)
+def photo(name):
+    flash_light.on()
+    img=camera.capture()
+    flash_light.off()
+    with open(name,'w') as f:
+        f.write(img)
